0f8e830c5aa3c5021c2a6ae1a5f5ee36a016fe4e
PSouttre <peggy.souttre@free.fr>
2023-11-06 21:26:50 +0100

    modale

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -3,6 +3,7 @@ import { displayWorks } from "./utils/dom.js";
 import { getCategories } from "./utils/fetch.js";
 import { displayCategories } from "./utils/dom.js";
 import { onClickFilters } from "./filters.js";
+import { displayWorksModale } from "./modale.js";
 
 // FICHIER D'INITIALISATION
 const init = async () => {
@@ -12,15 +13,16 @@ const init = async () => {
 
   displayWorks(works);
   displayCategories(categories);
+  displayWorksModale(works);
 
   onClickFilters();
 
   // 3 - si j'ai un token dans le localStorage
   // alors on affiche les boutons de modifications
-  if (localStorage.getItem("userLogin") != null) {
-    blackBanner.style.display = "block";
-    mesProjetsModeEdit.style.display = "block";
-  }
+  // if (localStorage.getItem("userLogin") != null) {
+  //   blackBanner.style.display = "block";
+  //   mesProjetsModeEdit.style.display = "block";
+  // }
 };
 
 // ============================================ //
--- FrontEnd/js/modale.js
+++ FrontEnd/js/modale.js
@@ -1,28 +1,44 @@
-export const onClickModifier = () => {
-  buttonModifier.addEventListener("click", () => {
-    //ouvrir la modale
-  });
+export const displayWorksModale = (works) => {
+  const galleryModale = document.querySelector(".galleryModale");
 
-  // buttonCategories.forEach((button) => {
-  //   // quand on click sur un bouton/filtre
-  //   button.addEventListener("click", async () => {
-  //     // on va récupérer tous les works dans l'API
-  //     const works = await getWorks();
-  //     // on récupère le categoryId du buton
-  //     const categoryId = button.getAttribute("categoryId");
-
-  //     // si categoryId est false alors on affiche tout
-  //     if (categoryId === "false") {
-  //       displayWorks(works);
-  //     } else {
-  //       // on filtre les works
-  //       const filteredWorks = works.filter((work) => {
-  //         return work.categoryId == categoryId;
-  //       });
-
-  //       // on les affiche
-  //       displayWorks(filteredWorks);
-  //     }
-  //   });
-  // });
+  // on boucle sur TOUS les works
+  for (let i = 0; i < works.length; i++) {
+    // créer une figure qui correspond à un work
+    const figureModale = `<figure>
+        <img src=${works[i].imageUrl} alt=${works[i].title}>
+      </figure>`;
+    // on insère dans la gallery
+    galleryModale.innerHTML += figureModale;
+  }
 };
+console.log(galleryModale);
+
+// export const onClickModifier = () => {
+
+//     buttonModifier.addEventListener("click", () => {
+//         //ouvrir la modale
+// });
+
+// buttonCategories.forEach((button) => {
+//   // quand on click sur un bouton/filtre
+//   button.addEventListener("click", async () => {
+//     // on va récupérer tous les works dans l'API
+//     const works = await getWorks();
+//     // on récupère le categoryId du buton
+//     const categoryId = button.getAttribute("categoryId");
+
+//     // si categoryId est false alors on affiche tout
+//     if (categoryId === "false") {
+//       displayWorks(works);
+//     } else {
+//       // on filtre les works
+//       const filteredWorks = works.filter((work) => {
+//         return work.categoryId == categoryId;
+//       });
+
+//       // on les affiche
+//       displayWorks(filteredWorks);
+//     }
+//   });
+// });
+//   };
